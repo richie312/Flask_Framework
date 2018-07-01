72e25f7f2985efafd0747a6b2fc710b5cbea3cf7
richie312 <richie.chatterjee31@gmail.com>
2018-07-01 10:11:16 +0530

    Added the files

---
+++ data.py
@@ -0,0 +1,27 @@
+def Articles():
+	articles=[
+	{
+	'id': 1,
+	'title': 'A study on Pollution emission with ggMap',
+	'Author': 'Aritra Chatterjee',
+	'Date': '28-06-2018'
+	},
+	{
+	'id': 2,
+	'title': 'Comparative Stock Analysis with Quandl',
+	'Author': 'Aritra Chatterjee',
+	'Date': '28-06-2018'
+	},
+	{
+	'id': 3,
+	'title': 'Seismic Activity with animation package in R',
+	'Author': 'Aritra Chatterjee',
+	'Date': '28-06-2018'
+	}
+	]
+
+	return articles
+
+
+	
+	
